c960bf880ac7e3ad5b8d3c2e3e71dc7483238502
Eirik Kvistad <eirik.kvistad@gmail.com>
2020-03-24 15:47:55 +0100

    there

--- src/components/MP_comps/boxcomp.js
+++ src/components/MP_comps/boxcomp.js
@@ -6,16 +6,18 @@ export default class BoxComp extends React.Component {
     componentDidMount(){
         console.log(this.props)
     }
-
+    tagshandler = (item, index) => {
+      return item.map(x => <p key={index}>{x}</p>)
+    }
     render() {
-
+        
         
         return (
           <div class="boxes">
 
               {this.props.data.map((item, idx) => <div class="box"><h2 key={idx}>{item.name}</h2>
                                                                       <h3 key={idx}>by {item.owner}</h3>
-                                                                      <p key={idx}>{item.tags}</p></div>)}
+                                                                      {this.tagshandler(item.tags, idx)}</div>)}
                                                                       
           </div>
         );
