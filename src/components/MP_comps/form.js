746d2c88f12d5c17e982ed3fa6183083a743fb52
Eirik Kvistad <eirik.kvistad@gmail.com>
2020-05-14 12:09:11 +0200

    ads

--- src/components/MP_comps/form.js
+++ src/components/MP_comps/form.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
+import {UserContext} from "../../UserContext.js"
 
 
 
 export default class Form extends React.Component {
 
+    static contextType = UserContext;
    
 
     constructor(props) {
@@ -14,38 +16,59 @@ export default class Form extends React.Component {
             author: "",
             startDate:"",
             endDate:"",
-            noofstudents: "1"
+            noofstudents: "1",
+            keephidden: false,
+            postType: "",
+            description: "",
 
 
         };
 
         this.handleChange = this.handleChange.bind(this);
         
+        
     }
 
-    /* handleChange(event) {
-        this.setState({value: event.target.value});
-      } */
 
-    getRadiobuttonState(){
-        return {
-          selectedOption: 'internship'
-        };
-      }
+
+   
     
     handleChange(event){
         const target = event.target;
-        const value = target.name === 'isStudent' || 'keepHidden' ? target.checked : target.value;
+        const value = target.name === 'keepHidden' ? target.checked : target.value;
         const name = target.name;
 
         this.setState({
-            [name]: value,
-            selectedOption: event.target.value
+            [name]: value
+         
         });
     }
 
     
+    submitForm =(e) => {
+        e.preventDefault()
 
+        let user = this.context.value
+
+        let data = this.state;
+        
+        const userData = user.hasOwnProperty("contactName") ? {"name": user.name} : user.hasOwnProperty("employeeNo") ? {"employeeNo": user.employeeNo} : user.hasOwnProperty("studentNo") ? {"studentNo" : user.studentNo} : "reguser"
+
+        const key = Object.keys(userData)
+
+        data[key] = userData[key]
+        
+        
+        console.log(data)
+
+
+
+        // fetchData(data)
+        // .then(res => res.json())
+        // .then(result => {
+        //     console.log(result)
+        // })
+    }
 
 
     render(){
@@ -59,7 +82,7 @@ export default class Form extends React.Component {
                             <br /><input 
                                 name="title"
                                 type="text"
-                                value={this.state.value}
+                                value={this.state.title}
                                 onChange={this.handleChange}/>
                         </label>
                     </div>
@@ -69,7 +92,7 @@ export default class Form extends React.Component {
                             <br /><input
                                 name="companyName"
                                 type="text"
-                                value={this.state.value}
+                                value={this.state.companyName}
                                 onChange={this.handleChange} />
                         </label>
                     </div>
@@ -79,7 +102,7 @@ export default class Form extends React.Component {
                             <br /><input
                                 name="author"
                                 type="text"
-                                value={this.state.value}
+                                value={this.state.author}
                                 onChange={this.handleChange}/>
                         </label>
                     </div>
@@ -88,12 +111,12 @@ export default class Form extends React.Component {
                     <div class="formright">
                         
                         <label>
-                            <input type="radio" name="radiobuttons" value="internship" checked={this.state.selectedOption === 'internship'} onChange={this.handleChange}/>
+                            <input type="radio" name="postType" value="internship" checked={this.state.postType === 'internship'} onChange={this.handleChange}/>
                             Internship
                         </label>
                         
                         <label>
-                            <input type="radio" name="radiobuttons" value="projects" checked={this.state.selectedOption === 'projects'} onChange={this.handleChange}/>
+                            <input type="radio" name="postType" value="projects" checked={this.state.postType === 'projects'} onChange={this.handleChange}/>
                             Projects
                         </label>
                         
@@ -117,6 +140,8 @@ export default class Form extends React.Component {
                         <label>
                             Start date:
                             <br /><input 
+                                value={this.state.startDate}
+                                onChange={this.handleChange}
                                 name="startDate"
                                 type="date"/>
                         </label>
@@ -124,20 +149,22 @@ export default class Form extends React.Component {
                     <div class="formmiddle">
                         <label>
                             End date:
-                            <br /><input 
+                            <br /><input
+                                value={this.state.endDate}
+                                onChange={this.handleChange}
                                 name="endDate"
                                 type="date"/>
                         </label>
                     </div>
                     <div class="formright">
-                        <label>
+                        {/* <label>
                                 <input
                                     name="isStudent"
                                     type="checkbox"
                                     checked={this.state.isStudent}
                                     onChange={this.handleChange} />
                             I am a student
-                        </label>
+                        </label> */}
                         <label>
                                 <input
                                     name="keepHidden"
@@ -152,11 +179,11 @@ export default class Form extends React.Component {
                <div class="formdesc">
                     <label>
                         Description:
-                        <br /><textarea value={this.state.value} onChange={this.handleChange}></textarea>
+                        <br /><textarea name="desc" value={this.state.desc} onChange={this.handleChange}></textarea>
                     </label>
                 </div>
 
-                <input type="submit" id="formsubmit" value="Submit" name="submitform" />
+                <button  onClick={(e) => this.submitForm(e)} type="button" id="formsubmit" value="Submit" name="submitform" >Submit</button>
             </form>
         )
 
@@ -166,3 +193,22 @@ export default class Form extends React.Component {
   
 
 }
+
+
+const fetchData = async (userData, data) => {
+    
+   
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({  })
+    };
+    
+    const result = await fetch(
+      `http://192.168.64.3/php-aws-codepipeline/newPost.php?`, requestOptions,
+    );
+    
+    return result
+
+  };
