385e3a4148e79e25413a8a4d4b25d77c9ed121f1
Mari Rogne <marirosta@hotmail.com>
2020-05-31 19:15:06 +0200

    Changed colour to ntnu-blue, translated comments

--- src/components/endpoint.js
+++ src/components/endpoint.js
@@ -1,4 +1,4 @@
 import React from "react";
 //aws: http://ec2-13-48-129-131.eu-north-1.compute.amazonaws.com    -- http://192.168.64.3/php-aws-codepipeline
-const Endpoint = "http://192.168.64.3/php-aws-codepipeline"
+const Endpoint = "http://ec2-13-48-129-131.eu-north-1.compute.amazonaws.com"
 export default Endpoint
